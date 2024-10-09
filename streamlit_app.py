809e1f9e946ddde74d87348f95591e44eacd63ef
Vijay Rodrigues <vijayrodrigues18@gmail.com>
2024-10-09 20:05:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -48,6 +49,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
     
     
-    import requests
+    
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
     st.text(fruityvice_response.json())
